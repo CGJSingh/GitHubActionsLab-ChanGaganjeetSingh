1aba69517cf3eac3faae157c96330fc0ed12a082
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-11 22:52:04 -0500

    edited yaml file

---
+++ src/main/java/ca/sheridancollege/sinchang/ice5/ICE5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.sinchang.ice5;
+
+/**
+ *
+ * @author singh
+ */
+public class ICE5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
